8baf9acbb8b799b6b509c49cd74912b46e914e1d
Sasuke-003 <56964273+Sasuke-003@users.noreply.github.com>
2021-06-22 17:07:06 +0530

    rating align changed

--- src/Components/Ratings/Ratings.jsx
+++ src/Components/Ratings/Ratings.jsx
@@ -4,7 +4,7 @@ import GradeSharpIcon from "@material-ui/icons/GradeSharp";
 function Ratings({ rated = false }) {
     const [rate, setRate] = useState(0);
     // const [rate, setRate] = useState(0);
-    const [currentStar, setCurrentStar] = useState(6);
+    const [currentStar, setCurrentStar] = useState(-1);
 
     return rated ? (
         <div>
@@ -17,9 +17,9 @@ function Ratings({ rated = false }) {
             {[...Array(5)].map((elementInArray, index) => (
                 <GradeSharpIcon
                     onMouseEnter={() => setCurrentStar(index)}
-                    onMouseLeave={() => setCurrentStar(6)}
+                    onMouseLeave={() => setCurrentStar(-1)}
                     key={index + "orders" + index + elementInArray}
-                    style={{ marginLeft: "5px", fontSize: "40px", fill: `${currentStar > index ? "var(--border)" : "var(--secondary)"}` }}
+                    style={{ marginLeft: "5px", fontSize: "40px", fill: `${currentStar < index ? "var(--border)" : "var(--secondary)"}` }}
                 />
             ))}
         </div>
